757920cd9cc102d62759801a7b0a977c4e26cae4
sreenivas <sreenivas@DESKTOP-BMLLM5O>
2019-07-30 21:40:35 +0530

    New Test Monk

---
+++ PoRaDsk/src/test/java/NewMavenMaven/PoRaDsk/AuditNonElgible.java
@@ -0,0 +1,17 @@
+package NewMavenMaven.PoRaDsk;
+
+import org.testng.annotations.Test;
+
+public class AuditNonElgible {
+	@Test()
+	public void NewmacTest() {
+		
+		System.out.println("new none");
+	}
+	@Test()
+	public void Newmac123Test() {
+		
+		System.out.println("new second");
+	}
+
+}
